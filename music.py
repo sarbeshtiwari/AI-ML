c44b300ed6b0dcc7c3c674d9799a11cb17cfad56
Sarbesh Kumar Tiwari <89567003+sarbeshtiwari@users.noreply.github.com>
2024-03-24 23:08:53 +0530

    Initial Commit

---
+++ music.py
@@ -0,0 +1,145 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2 
+import numpy as np 
+import mediapipe as mp 
+from keras.models import load_model
+import webbrowser
+import requests
+from streamlit_player import st_player
+
+model  = load_model("model.h5")
+label = np.load("labels.npy")
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+holis = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+
+
+    # Set custom CSS for the gradient background
+st.markdown(
+        """
+        <style>
+        .stApp {
+                background: linear-gradient(to right, olive, red);
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def program():
+        
+        st.header("Emotion Based Music Recommender")
+
+        if "run" not in st.session_state:
+                st.session_state["run"] = "true"
+
+        try:
+                emotion = np.load("emotion.npy")[0]
+        except:
+                emotion=""
+
+        if not(emotion):
+                st.session_state["run"] = "true"
+        else:
+                st.session_state["run"] = "false"
+
+        class EmotionProcessor:
+                def recv(self, frame):
+                        frm = frame.to_ndarray(format="bgr24")
+
+		##############################
+                        frm = cv2.flip(frm, 1)
+
+                        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+
+                        lst = []
+
+                        if res.face_landmarks:
+                                for i in res.face_landmarks.landmark:
+                                        lst.append(i.x - res.face_landmarks.landmark[1].x)
+                                        lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+                                if res.left_hand_landmarks:
+                                        for i in res.left_hand_landmarks.landmark:
+                                                lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                                                lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+                                else:
+                                        for i in range(42):
+                                                lst.append(0.0)
+
+                                if res.right_hand_landmarks:
+                                        for i in res.right_hand_landmarks.landmark:
+                                                lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                                                lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+                                else:
+                                        for i in range(42):
+                                                lst.append(0.0)
+
+                                lst = np.array(lst).reshape(1,-1)
+
+                                pred = label[np.argmax(model.predict(lst))]
+
+                                print(pred)
+                                cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+
+                                np.save("emotion.npy", np.array([pred]))
+
+			
+                        drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+								landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
+								connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+                        drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+                        drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+
+		##############################
+
+                        return av.VideoFrame.from_ndarray(frm, format="bgr24")
+
+        lang = st.text_input("Language")
+        singer = st.text_input("Singer")
+
+        if lang and singer and st.session_state["run"] != "false":
+                webrtc_streamer(key="key", desired_playing_state=True,
+                			video_processor_factory=EmotionProcessor)
+
+        btn = st.button("Recommend me songs")
+
+        if btn:
+            if not(emotion):
+                st.warning("Please let me capture your emotion first")
+                st.session_state["run"] = "true"
+            else:
+        # Use YouTube Data API to search for videos and get the video ID of the top result
+                youtube_data_api_key = 'AIzaSyDcnx509ugit4gUCp7m1926QplAN_5MnuQ'
+                search_query = f"{lang} {emotion} song {singer}"
+                search_url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={search_query}&key={youtube_data_api_key}"
+                response = requests.get(search_url)
+                print(search_url)
+                data = response.json()
+                if data['items']:
+                    for item in data['items']:
+                        if 'id' in item and 'videoId' in item['id']:
+                            video_id = item['id']['videoId']
+        
+
+            # Play the video in an embedded player
+                            st_player(f"https://www.youtube.com/watch?v={video_id}")
+
+                            np.save("emotion.npy", np.array([""]))
+                            st.session_state["run"] = "false"
+                else:
+                    st.error("No videos found. Please try again.")
+
+#st.header("Welcome to the world of Emorsic")
+
+
+#if st.button("Continue"):
+program()
+
+
+
+
